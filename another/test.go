e069f195f9d33f8980ca5b6bfd921368dfed7954
Chang Sau Sheong <sausheong@gmail.com>
2017-03-15 21:00:49 +0800

    added test.go in another folder

---
+++ another/test.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	r "github.com/sausheong/red/responder/core"
+)
+
+func main() {
+	r.ROUTEID = "GET/_/hello/world"
+	r.RESPOND = respond
+	r.Run()
+}
+
+func respond(request r.RequestInfo, response *r.ResponseInfo) {
+	response.SetHTML()
+	response.Body = "Hello World  "
+}
